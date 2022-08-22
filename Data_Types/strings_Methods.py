7efe9d1aa5d27fd5cec0751449a26730d8e2dac6
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2022-08-21 20:26:02 -0400

    no message

--- Data_Types/strings_Methods.py
+++ Data_Types/strings_Methods.py
@@ -9,10 +9,14 @@
                               Note: If fillchar is not provided, whitespace is taken as the default argument.
 
                               It returns a string padded with specified fillchar.
+                              The center() method doesn't modify the original string.
+
+        3. String casefold() :- This method converts all characters of the string into lowercase letters and returns a new string.
 
 
 """
 
+## 1. capitalize()
 sentence = "i love PYTHON"
 # converts first character to uppercase and others to lowercase
 capitalized_string = sentence.capitalize()
@@ -21,3 +25,34 @@ print(capitalized_string)  # Output: I love python
 # the sentence string is not modified.
 print('Before capitalize():', sentence)  # Before capitalize(): i love PYTHON
 print('After capitalize():', capitalized_string)  # After capitalize(): I love python
+
+## 2. center()
+sentence = "Python is awesome"
+
+# returns the centered padded string of length 24
+new_string = sentence.center(24, '*')
+print(new_string)
+# Output: ***Python is awesome****
+
+#center() with Default Argument
+text = "Python is awesome"
+# returns padded string by adding whitespace up to length 24
+new_text = text.center(24)
+print("Centered String: ", new_text)   # Centered String:     Python is awesome
+
+
+## 3. String casefold() :- This method converts all characters of the string into lowercase letters and returns a new string.
+# The casefold() method is similar to the lower() method but it is more aggressive. This means the casefold() method converts more characters into lower case compared to lower() .
+
+text = "PYTHON IS FUN"
+
+# converts text to lowercase
+print(text.casefold())  #-> python is fun
+
+text = 'groß'
+
+# convert text to lowercase using casefold()
+print('Using casefold():', text.casefold())  # Using casefold(): gross
+
+# convert text to lowercase using lower()
+print('Using lower():', text.lower())  # Using lower(): groß
